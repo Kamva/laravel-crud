b00050412d463d9455bb31eca80db081d13ffb45
Mehdi Meshkatian <mmeshkatian@users.noreply.github.com>
2024-11-30 16:18:26 +0330

    update crud

--- src/CRUDController.php
+++ src/CRUDController.php
@@ -1,4 +1,4 @@
-<?php
+ذ<?php
 
 namespace Kamva\Crud;
 
@@ -381,15 +381,16 @@ class CRUDController extends Controller
 
         $this->applyFilters($request, $rows);
 
+         if (empty($rows->getQuery()->orders)) {
+                $rows->orderBy("created_at", "desc");
+            }
+            
         if ($request->has('export')) {
             return $this->exportData($rows);
         }
 
         if (KamvaCrud::isApi()) {
-            if (empty($rows->getQuery()->orders)) {
-                $rows->orderBy("created_at", "desc");
-            }
-
+        
             return $this->handleApiResponse($rows);
         } else {
             if ($request->wantsJson()) {
